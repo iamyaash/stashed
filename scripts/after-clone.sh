85a604378a883a5a5ede043eb87b5961194fe516
iamyaash <iamyaash@outlook.com>
2025-09-21 14:35:05 +0530

    chore: scripts update
    Signed-off-by: iamyaash <iamyaash@outlook.com>

--- scripts/after-clone.sh
+++ scripts/after-clone.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
 echo "Downloading PaperMod themes to the themes/ directory"
-git submodule add --depth=1 https://github.com/adityatelange/hugo-PaperMod.git themes/PaperMod
-git submodule update --init --recursive # needed when you reclone your repo (submodules may not get cloned automatically)
+# Remove existing submodule files and Git data
+rm -rf themes/PaperMod*
+rm -rf .git/modules/themes/PaperMod*
+
+# Remove the submodule entry from the index (if exists)
+git rm --cached -f themes/PaperMod*
+
+echo "Adding PaperMod themes to the themes/ directory"
+# git submodule add --depth=1 https://github.com/iamyaash/PaperModPlus.git themes/PaperModPlus
+git submodule add --depth=1 https://codeberg.org/dovah-kiin/PaperModPlus.git themes/PaperModPlus
+
+# Sync submodules config (safety)
+git submodule sync --recursive
+
+# Initialize and update submodules
+git submodule update --init --recursive
+
+echo "Done."
--- scripts/new-content.sh
+++ scripts/new-content.sh
@@ -5,3 +5,16 @@ read -p "Enter the location (e.g: dirName/filename.md): "  location
 
 echo "Creating a new content in content/posts/$location"
 hugo new content content/posts/"$location"
+
+# Insert CustomLine=2424 at 5th line
+file="content/posts/$location"
+tmpfile=$(mktemp)
+
+insert_lines='params:
+	author: "Yashwanth Rathakrishnan"
+	ShowToc: true
+	ShowReadingTime: true
+	ShowCodeCopyButtons: true'
+
+awk -v text="$insert_lines" 'NR==5{print text}1' "$file" > "$tmpfile" && mv "$tmpfile" "$file"
+
